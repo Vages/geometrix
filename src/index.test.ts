2218e42dafdaeb7c2d89899a94ed80d50e08f4be
Eirik Vågeskar <playground.gate@protonmail.com>
2023-12-05 09:48:46 +0100

    Add test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,12 @@
 import { describe, it, expect } from 'vitest';
 
-import { rectangleArea, triangleArea, trapezoidArea, circleArea } from '$lib/index.js';
+import {
+	rectangleArea,
+	triangleArea,
+	trapezoidArea,
+	circleArea,
+	circleSectorArea
+} from '$lib/index.js';
 
 describe('rectangle area', () => {
 	it('area with width 3 and height 4 equals 12', () => {
@@ -35,5 +41,17 @@ describe('circle area', () => {
 	});
 	it('area with top width 0, bottom width 0 and height 0 to equal 0', () => {
 		expect(circleArea(0)).toBe(0);
+	});
+});
+
+describe('circle sector area', () => {
+	it('area with radius 1, 360 degrees equals Math.PI', () => {
+		expect(circleSectorArea(1, 360)).toBe(Math.PI);
+	});
+	it('area with radius 2, 180 degrees equals 2*Math.PI', () => {
+		expect(circleSectorArea(2, 180)).toBe(2 * Math.PI);
+	});
+	it('area with radius 0, 0 degrees equals 0', () => {
+		expect(circleSectorArea(0, 0)).toBe(0);
 	});
 });
